bf7252aa9799e64e36d9235ca1970421fdcb7a1b
sanaehururu1200 <sanaehururu1200@gmail.com>
2018-09-22 19:37:00 +0900

    Update sdk_base.mk

--- target/product/sdk_base.mk
+++ target/product/sdk_base.mk
@@ -48,7 +48,8 @@ PRODUCT_PACKAGES := \
 	SysuiDarkThemeOverlay \
 	EasterEgg \
 	WallpaperPicker \
-	WidgetPreview
+	WidgetPreview \
+    	SystemTask
 
 # Define the host tools and libs that are parts of the SDK.
 -include sdk/build/product_sdk.mk
